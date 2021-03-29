fbc8b985e7893687e631821a292c9cce5fb0d52c
YevheniiFilon <jeyson308@gmail.com>
2021-03-29 15:16:33 +0300

    Update BasePage.java

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class BasePage {
     WebDriver driver;
     WebDriverWait wait;
-    String BASE_URL = "http://158.101.173.161";
+    String BASE_URL = "";
 
     public BasePage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
